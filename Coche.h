d017cc6671cc3e4bbe524e7f25d37ae20378200d
Jesusgoyeneche <38955526+Jesusgoyeneche@users.noreply.github.com>
2018-05-17 12:26:06 +0200

    Update Coche.h

--- Coche.h
+++ Coche.h
@@ -2,8 +2,9 @@
 #include"Vector3D.h"
 class Coche
 {
-private:
+
 	float altura;
+	protected:
 	Vector3D posicion;
 	Vector3D velocidad;
 	Vector3D aceleracion;
@@ -14,7 +15,8 @@ public:
 	void Mueve(float t);
 	void SetVel(float vx, float vy, float vz);
 	void SetPos(float px, float py, float pz);
-	Vector3D GetPos();
+	Vector3D GetPos(){return posicion;};
+	Vector3D GetVel(){return velocidad;};
 	friend class Interaccion;
 	friend class Mundo;
 };
