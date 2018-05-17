29c5fa248f081fca0bc82b41c5f115f788bb7627
Jesusgoyeneche <38955526+Jesusgoyeneche@users.noreply.github.com>
2018-05-17 12:26:39 +0200

    Update Interaccion.cpp

--- Interaccion.cpp
+++ Interaccion.cpp
@@ -10,14 +10,12 @@ Interaccion::Interaccion(void)
 Interaccion::~Interaccion(void)
 {
 }
-void Interaccion::choque(Coche &c,Bonus  b, Vector3D &o)
+void Interaccion::choque(Coche &c,Bonus  b)
 {
 Vector3D seg = c.posicion - b.posicion;
 	float dist = seg.modulo();
 	if(dist<=(c.altura + b.radio)){
-		c.velocidad=c.velocidad*1.25f;
-		o=c.velocidad;
+
+c.velocidad=c.velocidad*1.25f;
 	}
-
-
 }
